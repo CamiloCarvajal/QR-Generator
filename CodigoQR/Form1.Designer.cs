ac3532c180b716eeda7c869e685318a4f5816b06
Camilo Carvajal <camilo@talent.com>
2021-12-10 22:46:39 -0500

    Add project files.

---
+++ CodigoQR/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace CodigoQR
+{
+    partial class gencode
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(gencode));
+            this.generar = new System.Windows.Forms.Button();
+            this.guardar = new System.Windows.Forms.Button();
+            this.tbValor = new System.Windows.Forms.TextBox();
+            this.panelResultado = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // generar
+            // 
+            this.generar.BackColor = System.Drawing.Color.LightGray;
+            this.generar.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generar.Location = new System.Drawing.Point(19, 368);
+            this.generar.Margin = new System.Windows.Forms.Padding(1);
+            this.generar.Name = "generar";
+            this.generar.Size = new System.Drawing.Size(90, 35);
+            this.generar.TabIndex = 0;
+            this.generar.Text = "Generar";
+            this.generar.UseVisualStyleBackColor = false;
+            this.generar.Click += new System.EventHandler(this.generar_Click);
+            // 
+            // guardar
+            // 
+            this.guardar.BackColor = System.Drawing.Color.LightGray;
+            this.guardar.Enabled = false;
+            this.guardar.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guardar.Location = new System.Drawing.Point(111, 368);
+            this.guardar.Margin = new System.Windows.Forms.Padding(1);
+            this.guardar.Name = "guardar";
+            this.guardar.Size = new System.Drawing.Size(90, 35);
+            this.guardar.TabIndex = 1;
+            this.guardar.Text = "Guardar";
+            this.guardar.UseVisualStyleBackColor = false;
+            this.guardar.Click += new System.EventHandler(this.guardar_Click);
+            // 
+            // tbValor
+            // 
+            this.tbValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbValor.Location = new System.Drawing.Point(19, 325);
+            this.tbValor.Name = "tbValor";
+            this.tbValor.Size = new System.Drawing.Size(270, 22);
+            this.tbValor.TabIndex = 2;
+            // 
+            // panelResultado
+            // 
+            this.panelResultado.Location = new System.Drawing.Point(33, 18);
+            this.panelResultado.Name = "panelResultado";
+            this.panelResultado.Size = new System.Drawing.Size(240, 240);
+            this.panelResultado.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Goldenrod;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ingrese dirección URL";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Goldenrod;
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(-4, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(314, 154);
+            this.label2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(203, 368);
+            this.button1.Margin = new System.Windows.Forms.Padding(1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Limpiar";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gencode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(308, 427);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelResultado);
+            this.Controls.Add(this.tbValor);
+            this.Controls.Add(this.guardar);
+            this.Controls.Add(this.generar);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(324, 466);
+            this.MinimumSize = new System.Drawing.Size(324, 466);
+            this.Name = "gencode";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Generador código QR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generar;
+        private System.Windows.Forms.Button guardar;
+        private System.Windows.Forms.TextBox tbValor;
+        private System.Windows.Forms.Panel panelResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CodigoQR/Form1.cs
@@ -0,0 +1,66 @@
+﻿using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodigoQR
+{
+    public partial class gencode : Form
+    {
+        public gencode()
+        {
+            InitializeComponent();
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.MaximizeBox = false;
+        }
+
+        
+        private void generar_Click(object sender, EventArgs e)
+        {
+           
+                    QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+                    QrCode qrCode = new QrCode();
+                    qrEncoder.TryEncode(tbValor.Text, out qrCode);
+                    GraphicsRenderer renderer = new GraphicsRenderer(new FixedCodeSize(500, QuietZoneModules.Zero), Brushes.Black, Brushes.White);
+
+                    MemoryStream ms = new MemoryStream();
+                    renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, ms);
+                    var imageTemporal = new Bitmap(ms);
+                    var imagen = new Bitmap(imageTemporal, new Size(new Point(240, 240)));
+                    panelResultado.BackgroundImage = imagen;
+
+            //imagen.Save("imagen.png", ImageFormat.Png);
+            guardar.Enabled = true;
+        }
+
+        private void guardar_Click(object sender, EventArgs e)
+        {
+            Image imgFinal = (Image)panelResultado.BackgroundImage.Clone();
+
+            SaveFileDialog CajaDeDiaologoGuardar = new SaveFileDialog();
+            CajaDeDiaologoGuardar.AddExtension = true;
+            CajaDeDiaologoGuardar.Filter = "Image PNG (*.png)|*.png";
+            CajaDeDiaologoGuardar.ShowDialog();
+            if (!string.IsNullOrEmpty(CajaDeDiaologoGuardar.FileName))
+            {
+                imgFinal.Save(CajaDeDiaologoGuardar.FileName, ImageFormat.Png);
+            }
+            imgFinal.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tbValor.Clear();
+            tbValor.Focus();
+        }
+    }
+}
